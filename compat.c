c2d4766c294e44d63fffea4e1ea21e33c29d18ed
Brandon Casey <drafnel@gmail.com>
2025-08-23 14:55:23 -0500

    compat.c: add arc4random_buf() as a fallback RNG
    If getrandom() is not available, check if arc4random_buf() is available
    before falling back to reading directly from /dev/*random.

--- compat.c
+++ compat.c
@@ -5,14 +5,22 @@
  */
 #include "compat.h"
 
+#include <stdlib.h>       /* arc4random_buf */
 #include <fcntl.h>        /* open */
 #include <unistd.h>       /* read, close */
 
+#ifdef HAVE_BSD_STDLIB_H
+#include <bsd/stdlib.h>   /* arc4random_buf */
+#endif
 
 
 #ifndef HAVE_GETRANDOM
 ssize_t wds_getrandom (void *buf, size_t len, unsigned flags __attribute__((unused)))
 {
+#ifdef HAVE_ARC4RANDOM_BUF
+	arc4random_buf(buf, len);
+	return (ssize_t)len;
+#else
 	static const char* const rnd_devices[] = { "/dev/urandom", "/dev/random", NULL };
 	static const char * const *rnd_device = rnd_devices;
 	for ( ; *rnd_device; rnd_device++) {
@@ -27,5 +35,6 @@ ssize_t wds_getrandom (void *buf, size_t len, unsigned flags __attribute__((unus
 		return rd;
 	}
 	return -1;
+#endif
 }
 #endif /* HAVE_GETRANDOM */

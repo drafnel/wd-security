896526e407bb896558ca265f1a1ca2b9fef7c6ac
Brandon Casey <drafnel@gmail.com>
2026-03-17 22:14:41 -0500

    Use O_EXCL when opening device to send rescan ioctl
    On Linux, when a block device is opened with O_EXCL, the open will fail
    if the device is mounted.  Since we don't really want to trigger a
    partition rescan for a mounted device, which seems extremely unlikely to
    happen, let's open the device using O_EXCL when sending the BLKRRPART
    ioctl.

--- wd-security-cli.c
+++ wd-security-cli.c
@@ -127,7 +127,7 @@ static void hexdump (FILE *fp, const uint8_t *buf, size_t len) {
 static int reread_part (const char *device) {
 	int err;
 #ifdef HAVE_DECL_BLKRRPART
-	int fd = open(device, O_RDWR);
+	int fd = open(device, O_RDWR | O_EXCL);
 	if (fd == -1) {
 		perror("reread-part failed opening device");
 		return 1;
